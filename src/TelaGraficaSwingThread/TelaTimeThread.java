000c7d060a2070fb329c3870fbcbca78902a846e
Ssxund3r <gabrielfillip@gmail.com>
2022-05-15 19:51:59 -0300

    Controlando Threads 1 e 2 com Start e Stop

--- src/TelaGraficaSwingThread/TelaTimeThread.java
+++ src/TelaGraficaSwingThread/TelaTimeThread.java
@@ -5,6 +5,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -13,64 +17,139 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
-	
-	private JPanel jPanel = new JPanel(new GridBagLayout()); /*Painel de componentes*/
-	
+
+	private static final long serialVersionUID = 1L;
+
+	private JPanel jPanel = new JPanel(new GridBagLayout()); /* Painel de componentes */
+
 	private JLabel descricaoHora = new JLabel("Time Thread 1");
 	private JTextField mostraTempo = new JTextField();
-	
+
 	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
 	private JTextField mostraTempo2 = new JTextField();
-	
-	private JButton jButton = new JButton("Start"); 
-	private JButton jButton2 = new JButton("Stop"); 
- 	
-	public TelaTimeThread() { /*Executa o que estiver dentro no momeno da abertura ou execução*/
-		
+
+	private JButton jButton = new JButton("Start");
+	private JButton jButton2 = new JButton("Stop");
+
+	private Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			while (true) { /* Fica sempre rodando */
+				mostraTempo
+						.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+	};
+
+	private Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			while (true) { /* Fica sempre rodando */
+				mostraTempo2
+						.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+	};
+
+	private Thread threadTime1;
+	private Thread threadTime2;
+
+	public TelaTimeThread() { /* Executa o que estiver dentro no momento da abertura ou execução */
+
 		setTitle("Minha tela de time com Thread");
 		setSize(new Dimension(240, 240));
 		setLocationRelativeTo(null);
 		setResizable(false);
-		/*Primeira tela concluída*/
-		
-		GridBagConstraints bagConstraints = new GridBagConstraints(); /*Controlador de posicionamento de componentes*/
+		/* Primeira tela concluída */
+
+		GridBagConstraints bagConstraints = new GridBagConstraints(); /* Controlador de posicionamento de componentes */
 		bagConstraints.gridx = 0;
 		bagConstraints.gridy = 0;
 		bagConstraints.gridwidth = 2;
 		bagConstraints.insets = new Insets(5, 10, 5, 5);
-		
+
 		bagConstraints.anchor = GridBagConstraints.WEST;
-		
+
 		descricaoHora.setPreferredSize(new Dimension(200, 25));
 		jPanel.add(descricaoHora, bagConstraints);
-		
+
 		mostraTempo.setPreferredSize(new Dimension(200, 25));
 		bagConstraints.gridy++;
 		mostraTempo.setEditable(false);
-		jPanel.add(mostraTempo, bagConstraints); 
-		
+		jPanel.add(mostraTempo, bagConstraints);
+
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
 		bagConstraints.gridy++;
 		jPanel.add(descricaoHora2, bagConstraints);
-		
+
 		mostraTempo2.setPreferredSize(new Dimension(200, 25));
 		bagConstraints.gridy++;
 		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2, bagConstraints);
-		
+
 		bagConstraints.gridwidth = 1;
-		
+
 		jButton.setPreferredSize(new Dimension(92, 25));
 		bagConstraints.gridy++;
 		jPanel.add(jButton, bagConstraints);
-		
+
 		jButton2.setPreferredSize(new Dimension(92, 25));
 		bagConstraints.gridx++;
 		jPanel.add(jButton2, bagConstraints);
-		
+
+		jButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) { /* Executa o click no botão */
+
+				threadTime1 = new Thread(thread1);
+				threadTime1.start();
+
+				threadTime2 = new Thread(thread2);
+				threadTime2.start();
+
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
+
+			}
+		});
+
+		jButton2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				threadTime1.stop();
+				threadTime2.stop();
+
+				jButton.setEnabled(true);
+				jButton2.setEnabled(false);
+
+			}
+		});
+
+		jButton2.setEnabled(false);
+
 		add(jPanel, BorderLayout.WEST);
-		/*Sempre será o último comando*/
-		setVisible(true); /*Torna a tela visivel para o usuário*/
+		/* Sempre será o último comando */
+		setVisible(true); /* Torna a tela visivel para o usuário */
 	}
 
 }
