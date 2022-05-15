135c0e380586148096d26cc0e4e9c174e414dfdd
Ssxund3r <gabrielfillip@gmail.com>
2022-05-15 20:24:59 -0300

    Criando a fila e pilha para processamento paralelo em Thread

---
+++ src/PilhaParaleloThread/FilaThreadImpl.java
@@ -0,0 +1,48 @@
+package PilhaParaleloThread;
+
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class FilaThreadImpl extends Thread {
+
+	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilhaFila = new ConcurrentLinkedQueue<ObjetoFilaThread>();
+
+	public static void add(ObjetoFilaThread objetoFilaThread) {
+		pilhaFila.add(objetoFilaThread);
+	}
+
+	@Override
+	public void run() {
+		Iterator iteracao = pilhaFila.iterator();
+
+		synchronized (iteracao) { /*Bloquear o acesso a esta lista por outro processos*/
+
+			while (iteracao.hasNext()) { /* Enquanto conter dados na lista irá processar */
+
+				ObjetoFilaThread processar = (ObjetoFilaThread) iteracao.next(); /* Pega o objeto atual */
+
+				/*
+				 * Processar 10k notas fiscais | Gerar uma lista enorme de PDF| 
+				 * Gerar um envio em massa de email
+				 */
+				System.out.println("------------------");
+				System.out.println(processar.getEmail());
+				System.out.println(processar.getNome());
+				iteracao.remove();
+
+				try {
+					Thread.sleep(100); /* Dar um tempo para descarga de memoria */
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+
+		try {
+			Thread.sleep(1000); /* Processou toda a lista dar um tempo para limpeza de memória */
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
