ac14ddd86b94bb6e1e1ad85154df24616de0386f
Ssxund3r <gabrielfillip@gmail.com>
2022-05-15 15:46:25 -0300

    Thread em objeto e Runnable

--- src/Thread/ExemploThread.java
+++ src/Thread/ExemploThread.java
@@ -5,55 +5,18 @@ import javax.swing.JOptionPane;
 public class ExemploThread {
 
 	public static void main(String[] args) throws InterruptedException {
-		
-		/* Thread processando em paralelo - envio de e-mail*/
-		new Thread() {
-			public void run() { /* Executa o que queremos */
-				/* Código da rotina */
 
-				/* Código da rotina para executar em paralelo */
-				for (int pos = 0; pos < 10; pos++) {
+		/* Thread processando em paralelo - envio de e-mail */
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
-					/* Executar este envio com um tempo de parada, ou com um tempo determinador */
-					System.out.println("Executando alguma rotina. EX: envio de e-mail...");
-					try {
-						Thread.sleep(2000); /* Aplica o tempo proposto */
-					} catch (InterruptedException e) {
-
-						e.printStackTrace();
-					}
-				}
-				/* Fim do código */
-
-			};
-		}.start(); /* Start loga a thread que processa paralelamente por trás */
-		
-		
 		/*-----------------------------------------------------DIVISÃO DAS THREADS-----------------------------------------------*/
-		
+
 		/* Thread processando em paralelo - envio nota fiscal */
-		new Thread() {
-			public void run() { /* Executa o que queremos */
-				/* Código da rotina */
+		Thread threadNotaFiscal = new Thread(thread2);
+		threadNotaFiscal.start();
 
-				/* Código da rotina para executar em paralelo */
-				for (int pos = 0; pos < 10; pos++) {
-
-					/* Executar este envio com um tempo de parada, ou com um tempo determinador */
-					System.out.println("Executando alguma rotina. EX: envio de NF-e...");
-					try {
-						Thread.sleep(4000); /* Aplica o tempo proposto */
-					} catch (InterruptedException e) {
-
-						e.printStackTrace();
-					}
-				}
-				/* Fim do código */
-
-			};
-		}.start(); /* Start loga a thread que processa paralelamente por trás */
 		
-
 		/* Código do sistema continua o fluxo de trabalho */
 		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE - THREAD!");
 		/*
@@ -62,4 +25,51 @@ public class ExemploThread {
 		 */
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário!!!");
 	}
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			/* Executa o que queremos */
+			/* Código da rotina */
+
+			/* Código da rotina para executar em paralelo */
+			for (int pos = 0; pos < 10; pos++) {
+
+				/* Executar este envio com um tempo de parada, ou com um tempo determinador */
+				System.out.println("Executando alguma rotina. EX: envio de e-mail...");
+				try {
+					Thread.sleep(2000); /* Aplica o tempo proposto */
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+				}
+			}
+			/* Fim do código */
+
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			/* Executa o que queremos */
+			/* Código da rotina */
+
+			/* Código da rotina para executar em paralelo */
+			for (int pos = 0; pos < 10; pos++) {
+
+				/* Executar este envio com um tempo de parada, ou com um tempo determinador */
+				System.out.println("Executando alguma rotina. EX: envio de NF-e...");
+				try {
+					Thread.sleep(4000); /* Aplica o tempo proposto */
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+				}
+			}
+			/* Fim do código */
+		}
+	};
 }
