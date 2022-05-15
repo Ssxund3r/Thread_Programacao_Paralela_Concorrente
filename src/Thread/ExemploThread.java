a7412477804d44c35c8f4197fb6eba9961cd1370
Ssxund3r <gabrielfillip@gmail.com>
2022-05-15 14:57:40 -0300

    Criando primeira Thread em paralelo

--- src/Thread/ExemploThread.java
+++ src/Thread/ExemploThread.java
@@ -1,13 +1,38 @@
 package Thread;
 
+import javax.swing.JOptionPane;
+
 public class ExemploThread {
 
 	public static void main(String[] args) throws InterruptedException {
+		/* Thread processando em paralelo */
+		new Thread() {
+			public void run() { /* Executa o que queremos */
+				/* Código da rotina */
 
-		for (int pos = 0; pos < 10; pos++) {
-			Thread.sleep(1000);
-			/* Executar este envio com um tempo de parada, ou com um tempo determinador */
-			System.out.println("Executando alguma rotina. EX: envio de e-mail...");
-		}
+				/* Código da rotina para executar em paralelo */
+				for (int pos = 0; pos < 10; pos++) {
+
+					/* Executar este envio com um tempo de parada, ou com um tempo determinador */
+					System.out.println("Executando alguma rotina. EX: envio de e-mail...");
+					try {
+						Thread.sleep(1000); /* Aplica o tempo proposto */
+					} catch (InterruptedException e) {
+
+						e.printStackTrace();
+					}
+				}
+				/* Fim do código */
+
+			};
+		}.start(); /* Start loga a thread que processa paralelamente por trás */
+
+		/* Código do sistema continua o fluxo de trabalho */
+		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE - THREAD!");
+		/*
+		 * Exemplo de fluxo do sistema -> Cadastro de venda, emissão de nota fiscal
+		 * etc....
+		 */
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário!!!");
 	}
 }
