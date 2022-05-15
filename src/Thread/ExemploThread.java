393ee96488e6994b61a112159f28bcd72fdd3cd4
Ssxund3r <gabrielfillip@gmail.com>
2022-05-15 14:41:23 -0300

    Exemplo Thread

---
+++ src/Thread/ExemploThread.java
@@ -0,0 +1,14 @@
+package Thread;
+
+public class ExemploThread {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		for (int pos = 0; pos < 10; pos++) {
+
+			Thread.sleep(1000);
+			/* Executar este envio com um tempo de parada, ou com um tempo determinador */
+			System.out.println("Executando alguma rotina. EX: envio de e-mail...");
+		}
+	}
+}
