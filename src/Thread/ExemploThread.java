68a550c5aed62e7bcef68fe5cb90c5895502ce01
Ssxund3r <gabrielfillip@gmail.com>
2022-05-15 15:23:00 -0300

    Processamento concorrente entre duas threads

--- src/Thread/ExemploThread.java
+++ src/Thread/ExemploThread.java
@@ -5,7 +5,8 @@ import javax.swing.JOptionPane;
 public class ExemploThread {
 
 	public static void main(String[] args) throws InterruptedException {
-		/* Thread processando em paralelo */
+		
+		/* Thread processando em paralelo - envio de e-mail*/
 		new Thread() {
 			public void run() { /* Executa o que queremos */
 				/* Código da rotina */
@@ -16,7 +17,7 @@ public class ExemploThread {
 					/* Executar este envio com um tempo de parada, ou com um tempo determinador */
 					System.out.println("Executando alguma rotina. EX: envio de e-mail...");
 					try {
-						Thread.sleep(1000); /* Aplica o tempo proposto */
+						Thread.sleep(2000); /* Aplica o tempo proposto */
 					} catch (InterruptedException e) {
 
 						e.printStackTrace();
@@ -26,6 +27,32 @@ public class ExemploThread {
 
 			};
 		}.start(); /* Start loga a thread que processa paralelamente por trás */
+		
+		
+		/*-----------------------------------------------------DIVISÃO DAS THREADS-----------------------------------------------*/
+		
+		/* Thread processando em paralelo - envio nota fiscal */
+		new Thread() {
+			public void run() { /* Executa o que queremos */
+				/* Código da rotina */
+
+				/* Código da rotina para executar em paralelo */
+				for (int pos = 0; pos < 10; pos++) {
+
+					/* Executar este envio com um tempo de parada, ou com um tempo determinador */
+					System.out.println("Executando alguma rotina. EX: envio de NF-e...");
+					try {
+						Thread.sleep(4000); /* Aplica o tempo proposto */
+					} catch (InterruptedException e) {
+
+						e.printStackTrace();
+					}
+				}
+				/* Fim do código */
+
+			};
+		}.start(); /* Start loga a thread que processa paralelamente por trás */
+		
 
 		/* Código do sistema continua o fluxo de trabalho */
 		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE - THREAD!");
